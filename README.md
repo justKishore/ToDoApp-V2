3966a78614c87e4285303db3ac19e060a8de5e9d
Kishore S <skishore1843@gmail.com>
2022-06-21 19:10:37 +0530

    3. Get date in App.js, Scriptlet in List.ejs, Update README.md
    Finding date and day using switch case in App.js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Templating
 
 1. EJS Templating
+
+Refer [Docs](https://ejs.co/#docs)
+
+List.ejs is a template file, which contains html for ejs
--- app.js
+++ app.js
@@ -8,7 +8,39 @@ const app = express();
 app.set("view engine", "ejs"); // ejs documentation
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello</h1>");
+  var today = new Date();
+
+  var currentDay = today.getDay();
+
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      day =
+        "Oops something went wrong! But we are pretty sure its either weekday or weekend.";
+  }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
